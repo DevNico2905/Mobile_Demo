a13c53e49083bff19a7db32a7f6d3cd28f6e8926
DevNico2905 <nbernal36@unisalle.edu.co>
2026-05-21 15:49:39 -0500

    feat: implement login screen UI with provider integration and navigation links

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../core/app_theme.dart';
+import '../providers/login_provider.dart';
+import 'home_screen.dart';
+import 'registration_screen.dart';
+import '../widget/social_buttons.dart';
