fecd1e2bba4cd07dd2d2d0553dc30a21e760903c
DevNico2905 <nbernal36@unisalle.edu.co>
2026-05-21 15:55:06 -0500

    feat: implement basic structure and state management for LoginScreen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,3 +5,21 @@ import '../providers/login_provider.dart';
 import 'home_screen.dart';
 import 'registration_screen.dart';
 import '../widget/social_buttons.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();  
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    final provider = context.watch<LoginProvider>();
+
+
+    return Scaffold(
