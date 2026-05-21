499c724332f087e19d723477fa0ca3ab4f26ddd9
DevNico2905 <nbernal36@unisalle.edu.co>
2026-05-21 16:00:17 -0500

    feat: implement RegistrationScreen widget structure with form controllers and state variables

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -4,3 +4,19 @@ import '../core/validatiors.dart';
 import '../service/auth_services.dart';
 import '../screens/login_screen.dart';
 import '../widget/social_buttons.dart';
+
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _fullNameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _obscurePassword = true;
+  bool _obscureConfirmPassword = true;
+  bool _isLoading = false;
