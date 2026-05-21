984a350380b2c909c97c018a1255e5191e3a8180
DevNico2905 <nbernal36@unisalle.edu.co>
2026-05-21 15:59:47 -0500

    feat: implement registration screen with authentication and social login support

---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import '../core/app_theme.dart';
+import '../core/validatiors.dart';
+import '../service/auth_services.dart';
+import '../screens/login_screen.dart';
+import '../widget/social_buttons.dart';
